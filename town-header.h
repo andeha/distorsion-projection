1af4c8f45184fabddab6ca2b207546750ac83b6c
Anders Hansson <anders.pm.hansson@gmail.com>
2022-06-03 21:13:23 +0200

    Improved exposition.

---
+++ town-header.h
@@ -0,0 +1,15 @@
+/*  town-header.h | collection of mathematical functions available in libTown.a. */
+
+simd_tᵦ sin(simd_tᵦ x, int arc) ⓣ;
+Sequenta sin(Sequenta x, int arc) ⓣ;
+simd_tᵦ cos(simd_tᵦ x, int arc) ⓣ;
+Sequenta cos(Sequenta x, int arc) ⓣ;
+simd_tᵦ tan(simd_tᵦ x, int arc) ⓣ;
+Sequenta tan(Sequenta x, int arc) ⓣ;
+simd_tᵦ sqrt(simd_tᵦ x, int invert, simd_tᵦ * NoNthRootOrΨΛΩ) ⓣ;
+Sequenta sqrt(Sequenta x, int invert, Sequenta * NoNthRootOrΨΛΩ) ⓣ;
+simd_tᵦ power(simd_tᵦ x, int y₋equals₋10, int and₋inverted, simd_tᵦ y) ⓣ;
+Sequenta power(Sequenta x, int y₋equals₋10, int and₋inverted, Sequenta y) ⓣ;
+simd_tᵦ reciproc(simd_tᵦ x, int no₋exponential, int ₋and₋again₋inverted) ⓣ;
+Sequenta reciproc(Sequenta x, int no₋exponential, int ₋and₋again₋inverted) ⓣ;
+
