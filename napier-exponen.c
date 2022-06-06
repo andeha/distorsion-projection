14854eb3b803ddbfe57a51e886d8a9797d6bbfb6
Anders Hansson <anders.pm.hansson@gmail.com>
2022-06-06 21:38:51 +0200

    Improved exposition.

---
+++ napier-exponen.c
@@ -0,0 +1,27 @@
+/*  napier-exponen.c | simple and easy. */
+
+import Twinbeam;
+
+extern simd_tᵦ tanh(simd_tᵦ,int) ⓣ;
+extern Sequenta tanh(Sequenta,int) ⓣ;
+
+simd_tᵦ exponential(simd_tᵦ x) ⓣ
+{ simd_tᵦ one=simd_initᵦ(1.0),two=simd_initᵦ(2.0),param,eval,y,num,den;
+   param = __builtin_simd_divᵦ(x,two);
+   eval = tanh(param,0);
+   num = __builtin_simd_addᵦ(one,eval);
+   den = __builtin_simd_subᵦ(one,eval);
+   y = __builtin_simd_divᵦ(num,den);
+   return y;
+} /* (1+tanh(x/2))/(1-tanh(x/2)) */
+
+struct sequent exponential(Sequenta x) ⓣ
+{ Sequenta one=product₋abelian(), two=redundant₋many(),param,eval,y,num,den;
+   param = __builtin_fixpoint_div(x,two);
+   eval = tanh(param,0);
+   num = __builtin_fixpoint_add(one,eval);
+   den = __builtin_fixpoint_sub(one,eval);
+   y = __builtin_fixpoint_div(num,den);
+   return y;
+}
+
