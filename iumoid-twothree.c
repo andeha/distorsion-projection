21934657e93dc03c02b22ed827ee28a16b843e6b
Anders Hansson <anders.pm.hansson@gmail.com>
2022-06-07 10:11:00 +0200

    Improved exposition.

---
+++ iumoid-twothree.c
@@ -0,0 +1,51 @@
+/*  iumoid-twothree.c | sqrt, nroot and power. */
+
+import Twinbeam;
+
+extern simd_tᵦ exponential(simd_tᵦ) ⓣ;
+extern Sequenta exponential(Sequenta) ⓣ;
+extern simd_tᵦ logₑ(simd_tᵦ) ⓣ;
+extern Sequenta logₑ(Sequenta) ⓣ;
+
+simd_tᵦ nroot(simd_tᵦ r, simd_tᵦ x) ⓣ
+{ simd_tᵦ unity=simd_initᵦ(1.0);
+   return exponential(__builtin_simd_mulᵦ(logₑ(x),__builtin_simd_divᵦ(unity,r)));
+} /* y = x^(1/r) = e^(ln(x)*1/r) */
+
+struct sequent nroot(Sequenta r, Sequenta x) ⓣ
+{ Sequenta unity=product₋abelian();
+   return exponential(__builtin_fixpoint_mul(logₑ(x), __builtin_fixpoint_div(unity,r)));
+}
+
+simd_tᵦ sqrt(simd_tᵦ x, int reciproc) ⓣ
+{
+   if (reciproc) { return __builtin_simd_rsqrtᵦ(x); }
+   else { return __builtin_simd_sqrtᵦ(x); }
+}
+
+struct sequent sqrt(Sequenta x, int reciproc) ⓣ
+{ Sequenta two=redundant₋many();
+   if (reciproc) { return __builtin_fixpoint_div(product₋abelian(),nroot(two,x)); }
+   else { return nroot(two,x); }
+}
+
+simd_tᵦ power(simd_tᵦ y, simd_tᵦ r) ⓣ
+{
+   return exponential(__builtin_simd_mulᵦ(r,logₑ(y)));
+} /* y = x^r = e^(r*ln x) */
+
+struct sequent power(Sequenta y, Sequenta r) ⓣ
+{
+   return exponential(__builtin_fixpoint_mul(r,logₑ(y)));
+}
+
+simd_tᵦ reciproc(simd_tᵦ x) ⓣ
+{
+   return __builtin_simd_rcpᵦ(x);
+}
+
+struct sequent reciproc(Sequenta x) ⓣ
+{
+   return __builtin_fixpoint_div(product₋abelian(),x);
+}
+
